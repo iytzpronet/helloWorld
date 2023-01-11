169c5ea2958e9105e5c69da18b891566b6905b53
iytzpronet <iytzpronet@gmail.com>
2023-01-11 00:46:03 -0300

    meu primeiro commit

---
+++ src/main/java/org/example/controller/minhaPrimeiraClasseJava.java
@@ -0,0 +1,9 @@
+package org.example.controller;
+
+public class minhaPrimeiraClasseJava {
+    public static void main(String[] args) {
+        System.out.println("minha primeira classe java");
+
+
+    }
+}
