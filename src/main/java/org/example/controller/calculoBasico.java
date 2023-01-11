158c3ea293888705e6a0e184a685fbcb4699889c
iytzpronet <iytzpronet@gmail.com>
2023-01-11 00:56:41 -0300

    minha primeira calculadora basica

---
+++ src/main/java/org/example/controller/calculoBasico.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+public class calculoBasico {
+    public static void main(String[] args) {
+        //soma
+        double x =10;
+        double y =20;
+
+        System.out.println(x+y);
+
+        //subtraçao
+        System.out.println(x-y);
+
+        //multiplicaçao
+        System.out.println(x*y);
+
+        //divisao
+        System.out.println(x/y);
+
+
+    }
+}
